a8f4e132b84ad95cfbd6f3d6f9b08239e5d601d5
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2017-03-17 18:52:58 -0300

    Added findMatchingPaths tests.

---
+++ src/__tests__/findMatchingPaths.js
@@ -0,0 +1,32 @@
+import { findMatchingPaths } from '../index'
+
+describe('[method] findMatchingPaths', () => {
+  const cached = {
+    'ROOT_QUERY': {
+      'refField1': { type: 'id', id: 'id1', generated: false },
+      'refField2': { type: 'id', id: 'id2', generated: false }
+    },
+    'id1': { 'f1': 'id1 field one value', 'f2': 'id1 field two value' },
+    'id2': { 'f1': 'id2 field one value', 'f2': 'id2 field two value' },
+    'id3': { 'f1': 'id3 field one value', 'f2': 'id3 field two value' },
+  }
+
+  const addPath = jest.fn()
+  const matcher = findMatchingPaths(cached, addPath)
+
+  it('should match root level keys', () => {
+    expect(matcher([], ['id1'])).toContainEqual(['id1'])
+    expect(matcher([], ['ROOT_QUERY'])).toContainEqual(['ROOT_QUERY'])
+    expect(matcher([], ['wrong-id']).length).toBe(0)
+  })
+
+  it('should match second level keys', () => {
+    expect(matcher([], ['ROOT_QUERY', 'refField1'])).toContainEqual(['ROOT_QUERY', 'refField1'])
+    expect(matcher([], ['ROOT_QUERY', 'wrong-field']).length).toBe(0)
+  })
+
+  it('should add paths on third level keys', () => {
+    matcher([], ['ROOT_QUERY', 'refField2', 'f1'])
+    expect(addPath).toHaveBeenCalledWith(['id2', 'f1'])
+  })
+})
