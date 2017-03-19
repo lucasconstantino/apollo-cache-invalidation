c2faad1aa1228d50910638e8affb1a61680ae3db
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2017-03-19 20:12:47 -0300

    Added apollo-client integration test.

--- src/__tests__/integration.js
+++ src/__tests__/integration.js
@@ -1,3 +1,74 @@
+import ApolloClient from 'apollo-client'
+import gql from 'graphql-tag'
+
+import { invalidateFields } from '../index'
+
+const dataIdFromObject = ({ __typename, id }) =>
+  !id || !__typename ? null : __typename + id
+
+const mockedClient = query => new ApolloClient({
+  ssrMode: true,
+  dataIdFromObject,
+  networkInterface: { query },
+})
+
+/*
+Reference schema:
+
+type Person {
+  id: ID!
+  name: String!
+  happy: Boolean
+}
+
+type Query {
+  happyPeople: [Person]
+}
+
+type Mutation {
+  winWorldCup: Boolean
+}
+
+schema {
+  query: Query
+  mutation: Mutation
+}
+ */
+
 describe('integration', () => {
-  it('should work with real apollo-client')
+  it('should clear cache on a real apollo-client', async () => {
+    const results = {
+      HappyPeople: {
+        happyPeople: [
+          { __typename: 'Person', id: 1, happy: true },
+          { __typename: 'Person', id: 2, happy: false },
+        ]
+      },
+      WinWorldCup: {
+        winWorldCup: true
+      }
+    }
+
+    const resolver = jest.fn(
+      async ({ operationName }) => ({ data: results[operationName] })
+    )
+
+    const client = mockedClient(resolver)
+    const query = gql`query HappyPeople { happyPeople { id, happy } }`
+    const mutation = gql`mutation WinWorldCup { winWorldCup }`
+    const update = invalidateFields((proxy) => [['ROOT_QUERY', 'happyPeople']])
+
+    await client.query({ query })
+    await client.mutate({ mutation })
+    await client.query({ query })
+
+    // // Second query should have been cached, therefore not called.
+    expect(resolver).toHaveBeenCalledTimes(2)
+
+    await client.mutate({ mutation, update })
+    await client.query({ query })
+
+    // Second query now should have been called.
+    expect(resolver).toHaveBeenCalledTimes(4)
+  })
 })
